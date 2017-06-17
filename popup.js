01fd79a1e77ae6dce7baad73bfb6a3d27c4c2165
voidSatisfaction <lourie@naver.com>
2017-06-17 23:54:39 +0900

    Feat(#4): add button onpress setting (#11)
    * feat: add chrome storage store
    
    * fix: purify global scope
    
    * feat: get item data form chrome storage using promise
    
    * feat: attach unique id on each item's form

--- popup.js
+++ popup.js
@@ -1,28 +1,67 @@
+/*
+
+store structure
+
+store = {
+  data: [
+    { name: string, text: string }
+  ]
+}
+
+*/
+
+/* ----------------------------------------------- */
+/* DB / CRUD */
+/* ----------------------------------------------- */
+
+(function() {
+  var newItemButton = document.querySelector("#new-item-button");
+
+  newItemButton.addEventListener("click", function() {
+    var name = document.querySelector("#new-item-name").value;
+    var text = document.querySelector("#new-item-text").value;
+    
+    saveNewItem({ name, text })
+      .then(renderItems)
+      .then(function() {
+        console.log('save and render is done');
+      });
+  });
+})();
+
 /* ----------------------------------------------- */
 /* HEADER */
 /* ----------------------------------------------- */
 
-var addItem = document.querySelector(".add-btn")
+(function() {
+  var addItem = document.querySelector(".add-btn")
 
-addItem.onclick = function() {
-  this.classList.toggle("active");
-  var panel = document.querySelector(".new-item");
-  if (panel.style.maxHeight) {
-    panel.style.maxHeight = null;
-  } else {
-    panel.style.maxHeight = panel.scrollHeight + "px";
-  }
-};
+  addItem.addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = document.querySelector(".new-item");
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+})();
 
 /* ----------------------------------------------- */
 /* ITEMS */
 /* ----------------------------------------------- */
 
-var items = document.querySelectorAll(".item-name");
-var i;
+(function() {
+  var section = document.querySelector("section.section-items");
 
-for (i = 0; i < items.length; i++) {
-  items[i].onclick = function(){
+  section.innerHTML
+})();
+
+(function() {
+  var items = document.querySelectorAll(".item-name");
+
+  items.forEach(function(item) {
+    item.addEventListener("click", function() {
       this.classList.toggle("active");
       var panel = this.nextElementSibling;
       if (panel.style.maxHeight) {
@@ -30,5 +69,104 @@ for (i = 0; i < items.length; i++) {
       } else {
           panel.style.maxHeight = panel.scrollHeight + "px";
       }
-  };
+    });
+  });
+})();
+
+/* ----------------------------------------------- */
+/* TOOLS */
+/* ----------------------------------------------- */
+
+function initialize() {
+  chrome.storage.sync.set({
+    data: [
+      /*
+      { name: ... , text: ... }
+      */
+    ]
+  })
 }
+
+function renderItems() {
+  function getItemString({ name, text, index }) {
+    return `
+        <div class="item">
+          <button class="item-name">${name}</button>
+          <div class="item-detail">
+            <div class="row">
+              <label for="item-detail-text-${index}" class="item-detail-text-label">Text</label>
+              <textarea id="item-detail-text-${index}" class="item-detail-text" rows="10">${text}</textarea>
+            </div>
+            <div class="row">
+              <p class="item-text-nums">characters : ${text.length}</p>
+            </div>
+            <div class="row">
+              <div id="edit-button-${index}" class="btn btn-default full">Edit</div>
+            </div>
+          </div>
+        </div>
+      `;
+  }
+
+  var getStore = new Promise(function(resolve, reject) {
+    chrome.storage.sync.get(function(store) {
+      resolve(store);
+    });
+  });
+
+  return getStore
+    .catch(function(err) {
+      console.error('chrome storage error!');
+      console.error(err);
+    })
+    .then(function(store) {
+      var section = document.querySelector("section.section-items");
+      var items = '';
+      store.data.forEach(function(data, index) {
+        items += getItemString({
+          name: data.name,
+          text: data.text,
+          index
+        });
+      });
+      section.innerHTML = items;
+    })
+    .then(function() {
+      var items = document.querySelectorAll(".item-name");
+
+      items.forEach(function(item) {
+        item.addEventListener("click", function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight) {
+              panel.style.maxHeight = null;
+          } else {
+              panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+        });
+      });
+    });
+}
+
+function saveNewItem({ name, text }) {
+  return new Promise(function(resolve, reject) {
+    chrome.storage.sync.get(function(store) {
+      store.data.push({ name, text });
+
+      store = Object.assign({},store);
+
+      chrome.storage.sync.set(store);
+
+      resolve(store);
+    });
+  })
+}
+
+/* This is main function start beginning */
+
+function main() {
+  initialize();
+  renderItems();
+};
+
+main();
