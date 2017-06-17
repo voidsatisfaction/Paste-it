0f9b1b38d4ab399682bcb426da62700a6ecfd105
voidSatisfaction <lourie@naver.com>
2017-06-17 23:39:09 +0900

    feat: get item data form chrome storage using promise

--- popup.js
+++ popup.js
@@ -1,3 +1,15 @@
+/*
+
+store structure
+
+store = {
+  data: [
+    { name: string, text: string }
+  ]
+}
+
+*/
+
 /* ----------------------------------------------- */
 /* DB / CRUD */
 /* ----------------------------------------------- */
@@ -9,9 +21,13 @@
     var name = document.querySelector("#new-item-name").value;
     var text = document.querySelector("#new-item-text").value;
     
-    saveNewItem({ name, text });
+    saveNewItem({ name, text })
+      .then(renderItems)
+      .then(function() {
+        console.log('save and render is done');
+      });
   });
-})()
+})();
 
 /* ----------------------------------------------- */
 /* HEADER */
@@ -28,11 +44,17 @@
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
-})()
+})();
 
 /* ----------------------------------------------- */
 /* ITEMS */
 /* ----------------------------------------------- */
+
+(function() {
+  var section = document.querySelector("section.section-items");
+
+  section.innerHTML
+})();
 
 (function() {
   var items = document.querySelectorAll(".item-name");
@@ -48,7 +70,7 @@
       }
     });
   });
-})()
+})();
 
 /* ----------------------------------------------- */
 /* TOOLS */
@@ -64,15 +86,79 @@ function initialize() {
   })
 }
 
-function saveNewItem({ name, text }) {
-  chrome.storage.sync.get(function(store) {
-    store.data.push({ name, text });
+function renderItems() {
+  function getItemString({ name, text }) {
+    return `
+        <div class="item">
+          <button class="item-name">${name}</button>
+          <div class="item-detail">
+            <div class="row">
+              <label class="item-detail-text-label" for="item-detail-text">Text</label>
+              <textarea id="item-detail-text" class="item-detail-text" rows="10">
+                ${text}
+              </textarea>
+            </div>
+            <div class="row">
+              <p class="item-text-nums">characters : ${text.length}</p>
+            </div>
+            <div class="row">
+              <div class="btn btn-default full save-item">Edit</div>
+            </div>
+          </div>
+        </div>
+      `;
+  }
 
-    store = Object.assign({},store);
-
-    console.log(store);
-    chrome.storage.sync.set(store);
+  var getStore = new Promise(function(resolve, reject) {
+    chrome.storage.sync.get(function(store) {
+      resolve(store);
+    });
   });
+
+  return getStore
+    .catch(function(err) {
+      console.error('chrome storage error!');
+      console.error(err);
+    })
+    .then(function(store) {
+      var section = document.querySelector("section.section-items");
+      var items = '';
+      store.data.forEach(function(data) {
+        items += getItemString(data);
+      });
+      section.innerHTML = items;
+    })
+    .then(function() {
+      (function() {
+        var items = document.querySelectorAll(".item-name");
+
+        items.forEach(function(item) {
+          item.addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+          });
+        });
+      })();
+    })
+}
+
+function saveNewItem({ name, text }) {
+  return new Promise(function(resolve, reject) {
+    chrome.storage.sync.get(function(store) {
+      store.data.push({ name, text });
+
+      store = Object.assign({},store);
+
+      chrome.storage.sync.set(store);
+
+      resolve(store);
+    });
+  })
 }
 
 initialize();
