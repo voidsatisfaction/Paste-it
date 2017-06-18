8a553adb68388118f46adf7d73cf853f087e2151
voidSatisfaction <lourie@naver.com>
2017-06-18 21:49:53 +0900

    fix: delete not used codes

--- innerPage.js
+++ innerPage.js
@@ -70,25 +70,3 @@ function initialize() {
 
 initialize();
 listenPopup();
-
-// var contexts = ["page","selection","link","editable","image","video","audio"];
-// for (var i = 0; i < contexts.length; i++) {
-//   var context = contexts[i];
-//   var title = "Test '" + context + "' menu item";
-//   var id = chrome.contextMenus.create({
-//     title,
-//     "contexts": [context],
-//     "onclick": genericOnClick,
-//   });
-//   console.log("'" + context + "' item:" + id);
-// }
-
-
-// // Create a parent item and two children.
-// var parent = chrome.contextMenus.create({"title": "Test parent item"});
-// var child1 = chrome.contextMenus.create(
-//   {"title": "Child 1", "parentId": parent, "onclick": genericOnClick});
-// var child2 = chrome.contextMenus.create(
-//   {"title": "Child 2", "parentId": parent, "onclick": genericOnClick});
-// console.log("parent:" + parent + " child1:" + child1 + " child2:" + child2);
-
